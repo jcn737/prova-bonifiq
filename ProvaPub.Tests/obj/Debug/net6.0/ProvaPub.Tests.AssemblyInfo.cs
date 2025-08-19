5de5f3b3a4b49abfd085aadd4ac8b718686fbc41
jcn737 <jcn737@hotmail.com>
2025-08-19 10:37:39 -0300

    Movendo ProvaPub.sln para a raiz do repositório

--- ProvaPub.Tests/obj/Debug/net6.0/ProvaPub.Tests.AssemblyInfo.cs
+++ ProvaPub.Tests/obj/Debug/net6.0/ProvaPub.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProvaPub.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6012251fe6724f2f890fa9943d243abf562893")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProvaPub.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProvaPub.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
